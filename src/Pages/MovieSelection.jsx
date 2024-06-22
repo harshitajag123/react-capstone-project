c90c9031563bf77d1684dd35ba36cb521810a76c
harshitajag123 <jagtapharshita4@gmail.com>
2024-06-22 05:41:30 +0530

    setup react and routing

---
+++ src/Pages/MovieSelection.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function MovieSelection ()  {
+  return (
+    <div>MovieSelection</div>
+  )
+}
+
+export default MovieSelection
---
+++ src/main.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
+import Home from './Pages/Home.jsx';
+import Info from './Pages/Info.jsx';
+import Browse from './Pages/Browse.jsx';
+import MovieSelection from './Pages/MovieSelection.jsx';
+import NotFound from './Pages/404.jsx';
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+	<React.StrictMode>
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/info" element={<Info />} />
+				<Route path="/browse" element={<Browse />} />
+				<Route path="/selection" element={<MovieSelection />} />
+				<Route path="*" element={<NotFound />} />
+			</Routes>
+		</BrowserRouter>
+	</React.StrictMode>
+);
