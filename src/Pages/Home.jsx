d5db5cdc1b38a330acfcc03db641b73b8e688f16
harshitajag123 <jagtapharshita4@gmail.com>
2024-06-27 23:38:32 +0530

    Styling the SignUp page done

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -63,70 +63,173 @@ function Home() {
 			navigate("/selection");
 		}
 	};
+
+	//to style input fields
+	const getInputStyle = (field) => ({
+		color: "#7C7C7C",
+		height: "3vw",
+		marginLeft: "7vw",
+		marginRight: "7vw",
+		paddingLeft: "1vw",
+		backgroundColor: "#292929",
+		border: errors[field] ? "1px solid red" : "none",
+		borderRadius: "5px",
+	});
+
+	//to style error messages
+	const getErrorStyle = (field) => ({
+		color: "red",
+		marginLeft: "7vw",
+		marginRight: "7vw",
+		paddingLeft: "1vw",
+	});
+
 	return (
 		<div>
-			<form
-				onSubmit={handleSubmit}
+			<div
 				style={{
 					display: "flex",
-					flexDirection: "column",
-					gap: "20px",
-					width: "40vw",
-					marginLeft: "auto",
-					marginRight: "auto",
+					margin: "0",
+					padding: "0",
 				}}>
-				<input
-					type="text"
-					name="name"
-					placeholder="Name"
-					value={data.name}
-					onChange={handleInput}
+				<img
+					width="40%"
+					height="70%"
+					style={{ height: "70%" }}
+					src="src/images/signUp.png"
 				/>
-				<span style={{ color: "red" }}> {errors.name} </span>
-
-				<input
-					type="text"
-					name="username"
-					placeholder="Username"
-					value={data.username}
-					onChange={handleInput}
-				/>
-				<span style={{ color: "red" }}> {errors.username} </span>
-
-				<input
-					type="email"
-					name="email"
-					placeholder="Email"
-					value={data.email}
-					onChange={handleInput}
-				/>
-				<span style={{ color: "red" }}> {errors.email} </span>
-
-				<input
-					type="telephone"
-					name="mobile"
-					placeholder="Mobile"
-					value={data.mobile}
-					onChange={handleInput}
-				/>
-				<span style={{ color: "red" }}> {errors.mobile} </span>
-
-				<div>
+				<form
+					onSubmit={handleSubmit}
+					style={{
+						display: "flex",
+						flexDirection: "column",
+						gap: "10px",
+						//width: "54vw",
+						backgroundColor: "black",
+						width: "60%",
+					}}>
+					<h1
+						style={{
+							color: "white",
+							textAlign: "center",
+							fontFamily: "'Single Day', cursive",
+							color: "#72DB73",
+							fontWeight: "400",
+							fontSize: "5vw",
+							marginBottom: "0",
+						}}>
+						Super app
+					</h1>
+					<h2
+						style={{
+							color: "white",
+							textAlign: "center",
+							fontFamily: "DM Sans, sans-serif",
+							fontWeight: "400",
+							fontSize: "2vw",
+							letterSpacing: "0.2vw",
+							marginTop: "0",
+						}}>
+						Create your new account
+					</h2>
 					<input
-						type="checkbox"
-						name="checkbox"
-						id="checkbox"
-						checked={data.checkbox}
+						type="text"
+						name="name"
+						placeholder="Name"
+						value={data.name}
 						onChange={handleInput}
+						style={getInputStyle("name")}
 					/>
+					<span style={getErrorStyle("name")}>{errors.name}</span>
 
-					<label htmlFor="checkbox">
-						Share my registration data with Superapp
-					</label>
-				</div>
-				<span style={{ color: "red" }}> {errors.checkbox} </span>
-				<button type="submit">Submit</button>
-			</form>
+					<input
+						type="text"
+						name="username"
+						placeholder="Username"
+						value={data.username}
+						onChange={handleInput}
+						style={getInputStyle("name")}
+					/>
+					<span style={getErrorStyle("username")}>{errors.username}</span>
+
+					<input
+						type="email"
+						name="email"
+						placeholder="Email"
+						value={data.email}
+						onChange={handleInput}
+						style={getInputStyle("name")}
+					/>
+					<span style={getErrorStyle("email")}>{errors.email}</span>
+
+					<input
+						type="telephone"
+						name="mobile"
+						placeholder="Mobile"
+						value={data.mobile}
+						onChange={handleInput}
+						style={getInputStyle("name")}
+					/>
+					<span style={getErrorStyle("mobile")}> {errors.mobile} </span>
+
+					<div style={{ display: "flex", flexDirection: "row" }}>
+						<input
+							type="checkbox"
+							name="checkbox"
+							id="checkbox"
+							checked={data.checkbox}
+							onChange={handleInput}
+							style={{
+								color: "#7C7C7C",
+								height: "3vw",
+								marginLeft: "7vw",
+								backgroundColor: "#292929",
+								border: "none",
+							}}
+						/>
+
+						<label
+							htmlFor="checkbox"
+							style={{
+								color: "white",
+								fontFamily: "DM Sans, sans-serif",
+								fontWeight: "400",
+								fontSize: "1.5vw",
+								marginBottom: "0",
+								marginLeft: "1vw",
+								marginTop: "1vw",
+								fontSize: "1.5vw",
+								cursor: "pointer",
+
+								// paddingTop:'-5vw'
+							}}>
+							Share my registration data with Superapp
+						</label>
+					</div>
+					<span style={getErrorStyle("checkbox")}> {errors.checkbox} </span>
+					<button
+						style={{
+							borderRadius: "5vw",
+							backgroundColor: "#72DB73",
+							color: "white",
+							fontFamily: "DM Sans, sans-serif",
+							fontWeight: "400",
+							fontSize: "1.5vw",
+							letterSpacing: "0.2vw",
+							marginLeft: "7vw",
+							marginRight: "7vw",
+							paddingLeft: "1vw",
+							paddingRight: "1vw",
+							padding: "1vw",
+							marginTop: "1vw",
+							marginBottom: "1vw",
+							cursor: "pointer",
+						}}
+						type="submit">
+						SIGN UP
+					</button>
+				</form>
+			</div>
 		</div>
 	);
 }
--- src/Pages/Info.jsx
+++ src/Pages/Info.jsx
@@ -1,9 +1,69 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { useState } from "react";
+function Info() {
+	const [notes, saveNotes] = useState(localStorage.getItem("notes") || "");
+	const [news, setNews] = useState([]);
+	const updateNotes = (e) => {
+		saveNotes(e.target.value);
+		localStorage.setItem("notes", JSON.stringify(e.target.value));
+	};
 
-function Info(){
-  return(
-    <div>Info</div>
-  )
+	const userData = JSON.parse(localStorage.getItem("userData"));
+	const selectedMovies = JSON.parse(localStorage.getItem("selectedMovies"));
+
+	useEffect(() => {
+		fetch("https://saurav.tech/NewsAPI/top-headlines/category/health/in.json")
+			.then((res) => res.json())
+			.then((data) => setNews(data.articles[Math.random()*data.articles.length | 0]));
+	}, []);
+  console.log(news);
+	return (
+		<div>
+			<div
+				style={{
+					display: "flex",
+					color: "white",
+					flexDirection: "column",
+					height: "40vh",
+					backgroundColor: "#5746EA",
+				}}>
+				{userData ? (
+					<>
+						<p>{userData.name}</p>
+						<p>{userData.email}</p>
+						<p>{userData.username}</p>
+						{/* <p>{userData.mobile}</p> */}
+					</>
+				) : (
+					"No user data"
+				)}
+
+				{selectedMovies ? (
+					<div
+						style={{ display: "grid", gridTemplateColumns: "repeat(2, 1fr)" }}>
+						{selectedMovies.map((movie, index) => (
+							<p key={index}>{movie.movie}</p>
+						))}
+					</div>
+				) : (
+					"No movies selected"
+				)}
+			</div>
+			<textarea
+				style={{
+					maxHeight: "400px",
+					minHeight: "400px",
+					minwidth: "40%",
+					maxWidth: "40%",
+					backgroundColor: "#F1C75B",
+					borderRadius: "10px",
+					marginTop: "20px",
+				}}
+				value={notes}
+				onChange={updateNotes}></textarea>
+      
+		</div>
+	);
 }
 
-export default Info
+export default Info;
